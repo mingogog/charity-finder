53ba7f8f0123a01dc61cde00c8979ec64955468e
mingogog <mingogog@gmail.com>
2023-08-04 11:04:02 -0400

    update index.html icon and title

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import HomePage from './pages/Home'
+import CharityProfile from './pages/CharityProfile'
+import Header from './components/Header/Header'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link
+} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage /> 
+  },
+  {
+    path: '/charity/:id',
+    element: <CharityProfile /> 
+  },
+  {
+    path: '*',
+    element: <HomePage /> 
+  }  
+])
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Header />
+      <RouterProvider router={router} />
+    </div>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+console.log(import.meta.env.VITE_EVERY_ORG_API_KEY)
+console.log(import.meta.env.VITE_EVERY_ORG__SECRET_KEY)
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />
 )
