c193d3a8f09b2dbd08893a99aa69cce1e434ebaf
mingogog <mingogog@gmail.com>
2023-08-04 11:01:26 -0400

    added charity type and app icon

---
+++ src/types/Charity.ts
@@ -0,0 +1,11 @@
+type charity = {
+    description: string,
+    profileUrl: string,
+    logoUrl: string,
+    slug: string,
+    tags: string
+    name: string
+    ein: string
+}
+
+export default charity
